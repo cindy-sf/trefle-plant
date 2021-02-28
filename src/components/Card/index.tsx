e9511aa084f1f64dc629cfd9498955d6b8ee6738
cindy-sf <c.saintfleurant@gmail.com>
2021-02-28 11:56:45 +0100

    feat: add redirection to plant details on search card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Link from 'next/link'
 
+import Image from '$src/components/Image'
 import Text from '$src/components/Text'
 import type { Plant } from '$src/types/plant'
 
@@ -10,9 +12,11 @@ import styles from './styles.module.scss'
 export const PlantCard:React.FC<{ plant: Plant }> = ({ plant }) => (
   <article>
     <article className={styles.plantCard}>
-      <div className={styles.plantImage}>
-        <img src={plant.image_url || fallBackPicure} alt={plant.common_name} />
-      </div>
+      <Link href={`/plant/${plant.id}`}>
+        <div className={styles.plantImage}>
+          <Image src={plant.image_url || fallBackPicure} alt={plant.common_name} />
+        </div>
+      </Link>
       <Text>{plant.common_name ||'Nom inconnu'}</Text>
       <Text>{plant.family}</Text>
     </article>
