4380dbf7a9fc19d1c6f88f84e185d30357e554b1
jonathansf <jonathan.saintfleurant@gmail.com>
2021-02-28 17:29:50 +0100

    styles: centered plant intro to the page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from 'react'
 import Router from 'next/router'
 
 import Head from 'next/head'
-import DotMenu from '$src/assets/images/home/dots-menu.png'
 
 import styles from './styles.module.scss'
 
@@ -18,16 +17,12 @@ const Layout:React.FC<Props> = ({ children, title = 'Trefle Plant' }) => (
     </Head>
     <header>
       <nav className={styles.nav}>
-        <span></span>
         <p
           className={styles.brandTitle}
           onClick={() => Router.push('/')}
         >
           Trefle Plant
         </p>
-        <button className={styles.dotMenu}>
-          <img src={DotMenu} alt="menu" />
-        </button>
       </nav>
     </header>
     {children}
